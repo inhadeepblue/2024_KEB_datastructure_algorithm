918ef1383fa61ded0bf8b894d67eadf41d747b4c
inhadeepblue <deepblue@inha.ac.kr>
2024-02-08 13:49:10 +0900

    v0.7 guess number

--- day20.py
+++ day20.py
@@ -1,9 +1,19 @@
-import mymath
-import time
+import random
 
-if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {mymath.nCr(n, r)}")
-    # f = int(input())
-    # print(mymath.factorial(f))
+answer = random.randint(1, 100)
+chance = 7
+
+while chance != 0:
+    guess = int(input("Input guess number : "))
+    if guess == answer:
+        print(f'You win. Answer is {answer}')
+        break
+    elif guess > answer:
+        chance = chance - 1
+        print(f'{guess} is bigger. Chance left : {chance}')
+    else:
+        chance = chance - 1
+        print(f'{guess} is lower. Chance left : {chance}')
+else:
+    print(f'You lost. Answer is {answer}')
+
