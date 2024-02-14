e24e03c329a860b209a1234000f8d3ec8691add6
inhadeepblue <deepblue@inha.ac.kr>
2024-02-14 16:54:10 +0900

    v1.7 fibonacci repetition

--- day22.py
+++ day22.py
@@ -12,7 +12,25 @@ def fibo_recursion(number: int) -> int:
         return fibo_recursion(number - 1) + fibo_recursion(number - 2)
 
 
+def fibo_repetition(number: int) -> int:
+    """
+    fibonacci function by repetition.
+    :param number: integer number
+    :return: integer number
+    """
+    a = 0
+    b = 1
+    for _ in range(number):
+        a, b = b, a + b
+    return a
+
+
 n = int(input("Input number : "))
+
 for i in range(0, n):
     print(i)
     print(fibo_recursion(i))
+
+for i in range(0, n):
+    print(i)
+    print(fibo_repetition(i))
